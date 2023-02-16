5c97bae967d01f9da086e76a8db6f44012d1b705
AceOne4 <ahmed.m.yassin14@gmail.com>
2023-02-16 13:41:15 -0800

    printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
  * main - A program that prints a line with puts function
- * Return : 0 (succes)
+ * Return : 0 (Succes passed)
+ * rutun (0) : main
 */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
-
-
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stadio.h>
+/**
+ * main-A program taht print a line using printf function 
+ * Return:0(Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
