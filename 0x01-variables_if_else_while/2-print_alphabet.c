fa3869dde79ea6de2a0ca531836bae81bc731138
AceOne4 <ahmed.m.yassin14@gmail.com>
2023-02-17 13:12:02 -0800

    3rd

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -  that is alphapeat game that shows all alphbaet
+ * main: prints alphabet
+ * Return: 0 (Succes)
+ */
+int main(void)
+{
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
