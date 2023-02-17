8eb108794496d914569af9760517290c4cc58ed1
AceOne4 <ahmed.m.yassin14@gmail.com>
2023-02-17 13:54:48 -0800

    8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main -  that is alphapeat game that shows all alphbaet
+ * main: prints alphabet
+ * Return: 0 (Succes)
+ */
+int main(void)
+{
+	int n;
+	char ch;
+
+	for(n = 48; n > 58; n++)
+	{
+		putchar(n)
+	}
+	for (ch = 'a'; ch <= 'f'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
