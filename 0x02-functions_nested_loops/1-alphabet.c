55f5a23ad6f5d52decc219a42c3cf3b935904c45
AceOne4 <ahmed.m.yassin14@gmail.com>
2023-02-21 12:35:48 -0800

    print alph

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - print all alphabeat in lower case
+ * print_alphabet: is function to print all alphabeat in lower case
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; a++)
+	{
+		_putchar(letter);
+	}
+	_putchar('\n');
+}
