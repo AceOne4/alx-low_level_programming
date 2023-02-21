238fd646dd9dc319e9fda88420bd52106a85a2ba
AceOne4 <ahmed.m.yassin14@gmail.com>
2023-02-21 14:48:57 -0800

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - function that add to integrs
+ * @a: is the first num to add
+ * @b:is second numbe to add
+ * Return: return the result
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
