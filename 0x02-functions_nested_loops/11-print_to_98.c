728308951c7b30e47a179e02ab1c3ff52750bca5
AceOne4 <ahmed.m.yassin14@gmail.com>
2023-02-21 15:34:37 -0800

    moduls

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,6 +14,7 @@ void print_to_98(int n)
 			{
 				printf("%d", n);
 				printf("\n");
+				break;
 			}
 			else
 			{
@@ -30,6 +31,7 @@ void print_to_98(int n)
 			{
 				printf("%d", n);
 				printf("\n");
+				break;
 			}
 			else
 			{
