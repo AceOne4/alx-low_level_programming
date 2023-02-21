7a8d68d5a837cc93955da198b985bcbf01688ce1
AceOne4 <ahmed.m.yassin14@gmail.com>
2023-02-21 14:35:12 -0800

    time tabel

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,18 +15,18 @@ void times_table(void)
 			{
 				u = z % 10;
 				d = (z - u) / 10;
-				_patchar(44);
-				_patchar(32);
-				_patchar(d + '0');
-				_patchar(u + '0');
+				_putchar(44);
+				_putchar(32);
+				_putchar(d + '0');
+				_putchar(u + '0');
 			}
 			else
 			{
 				if (y != 0)
 				{
 					_putchar(44);
-					_patchar(32);
-					_patchar(32);
+					_putchar(32);
+					_putchar(32);
 				}
 				_patchar(z + '0');
 			}
