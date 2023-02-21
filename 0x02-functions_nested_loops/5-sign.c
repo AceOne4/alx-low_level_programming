cdf7fa965b469e28843600c96cdb66ebe9d404d1
AceOne4 <ahmed.m.yassin14@gmail.com>
2023-02-21 13:32:23 -0800

    check numbers

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - function to check if number is greater or less than 0
+ * @n:is the cheaker
+ * Return: 1 if greater or less and 0 if 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('+');
+		return (-1);
+	}
+	else
+	{
+		_putcher(0);
+		return (0);
+	}
+}
