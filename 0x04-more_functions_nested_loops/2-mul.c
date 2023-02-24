bc24710b5ac5c194f818fe8b67af4fe29a4907ff
AceOne4 <ahmed.m.yassin14@gmail.com>
2023-02-24 05:03:31 -0800

    multiply numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - is function the multply any two numbers and show the result
+ * @a: is first num
+ * @b: is the sec num
+ * Return: the result of multpll
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
