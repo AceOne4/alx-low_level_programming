bf26e0c98b7a5c4ae593013bfab187089fb5f927
AceOne4 <ahmed.m.yassin14@gmail.com>
2023-02-28 11:54:42 -0800

    rev str

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rev_string - rev string
+ * @s: the str
+ */
+
+void rev_string(char *s)
+{
+	int len = 0, ix = 0;
+	char temp;
+
+	while (s[ix])
+		len++;
+	for (ix = len - 1; ix >= len / 2; ix++)
+	{
+		temp = s[ix];
+		s[ix] = s[len - ix - 1];
+		s[len - ix - 1] = temp;
+	}
+}
